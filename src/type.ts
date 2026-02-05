a8997fff9d5ad9addde70b1fa6b30534365a700d
Kevin Dean <Kevin.Dean@datadevelopment.com>
2026-02-05 18:05:37 -0500

    Refactored transformer call signatures to account for https://github.com/microsoft/TypeScript/pull/56941.

--- src/type.ts
+++ src/type.ts
@@ -7,17 +7,3 @@ export type TypeString = "string" | "number" | "boolean";
  * Map a string to its equivalent primitive type.
  */
 export type Primitive<T extends TypeString> = [T] extends ["string"] ? string : [T] extends ["number"] ? number : [T] extends ["boolean"] ? boolean : string | number | boolean;
-
-// TODO Review when https://github.com/microsoft/TypeScript/pull/56941 released.
-// Problem is in usage inside a function, e.g.,
-// function f<T extends number>(): void {
-//     const x: PrimitiveStringType<T> = "number";
-// }
-// /**
-//  * Map a primitive type to a string.
-//  */
-// type PrimitiveString<T extends string | number | boolean> =
-//     [T] extends [string] ? [string] extends [T] ? "string" : never :
-//         [T] extends [number] ? [number] extends [T] ? "number" : never :
-//             [T] extends [boolean] ? [boolean] extends [T] ? "boolean" : never :
-//                 never;
